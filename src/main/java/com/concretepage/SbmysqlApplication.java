33a15e6046e71a8551b8bef1b8c631e7302cc0b5
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-08-29 13:21:19 +0530

    First Commit

---
+++ src/main/java/com/concretepage/SbmysqlApplication.java
@@ -0,0 +1,12 @@
+package com.concretepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmysqlApplication.class, args);
+	}
+}
